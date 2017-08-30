82a82c688508525cf1c9fffe0f407a38355e4b0f
Alexander Fokin <AlexanderFokinDev@gmail.com>
2017-08-30 13:04:11 +0300

    Create a model of Knowledge base

--- knowledge_base/models.py
+++ knowledge_base/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class KnowledgeBase(models.Model):
+	id = models.IntegerField(primary_key=True)
+	question = models.TextField()
+	response = models.TextField()
