3da92b31c4cee2cef78c6d0479ec86ff4b3a3a34
Alexander Fokin <AlexanderFokinDev@gmail.com>
2017-08-30 15:32:35 +0300

    Added a view for the knowledge base

--- primple/urls.py
+++ primple/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from knowledge_base import views as knowledge_base_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^knowledge_base/$', knowledge_base_views.getList, name='getList')
 ]
